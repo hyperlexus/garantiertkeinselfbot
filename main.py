7d803e62efcd80d78af5e1dbf98b780ff7b7cdca
HyperLexus <you@example.com>
2024-12-16 19:43:27 +0100

    hab scheise gebaut

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import math
+from datetime import date
+from time import sleep
+
 from discord.ext import commands
 from dotenv import load_dotenv
 import os
@@ -9,15 +12,24 @@ TOKEN = os.getenv('TOKEN')
 
 bot = commands.Bot(command_prefix="bank.", self_bot=True)
 
+is_christmas_complete = False
+
 @bot.event
 async def on_ready():
     print(f"bot is running. logged in as {bot.user}")
 
 @bot.event
 async def on_message(message):
+    global is_christmas_complete
     if message.author == bot.user or message.author.bot or message.channel.id != 1317905858687930369:
         return
-    print(message.content)
+    user_id = message.author.id
+
+    if not is_christmas_complete and date.today().month == 12 and date.today().day == 24 and message.guild.id == 995966314877300737:
+        await message.channel.send("ES IST WEIHNACHTEN HURRUH")
+        for spast in (422800248935546880, 649457215564152852, 468786219258740756, 290148556776407050):
+            await message.channel.send(f"~tradeoffer <@{spast}> you_get: money: 1000")
+        is_christmas_complete = True
 
     def regerplatz(geld2, rate2):
         return math.ceil(geld2 ** 0.5 / 10) / (4 * rate2)
@@ -51,13 +63,58 @@ async def on_message(message):
             await message.channel.send(f"hast{' doppelt' if reingeschissen == 2 else ''} reingeschissen{', wie kann man nur so ein blöder spast sein' if reingeschissen == 2 else ''}.")
             return
 
-        user_id = message.author.id
         result = geld, rate, int(regerplatz(geld, rate) * 100), int(geld * (1 + regerplatz(geld, rate)))
         for i in result:
             print(i)
         await message.channel.send(f"~tradeoffer <@{user_id}> you_get: money: {geld} I_get: contract: perc: {rate}%; limit: {int(geld * (1 + regerplatz(geld, rate)))}")
 
-    if message.content.startswith("bank.help"):
-        await message.channel.send("`bank.offer <geld> <rückzahlrate in %>`\nExample: `bank.offer 100000 30`")
+    elif message.content.startswith("bank.help"):
+        await message.channel.send("die bank ist eine separate entity die geld hat und es für ihren eigenen zweck verleiht.\n"
+                                   "sie hat folgende commands:\n\n"
+                                   "bank.offer: leih dir geld von der bank, und spezifiziere die rate (% von income)\n"
+                                   "`bank.offer <geld> <rückzahlrate in %>`\nExample: `bank.offer 100000 30`\n\n"
+                                   "bank.donate: schenk der bank geld\n`bank.donate <geld>`\nExample: `bank.donate 10000`\n\n"
+                                   "bank.take: nimm geld aus der bank raus. dürfen nur alex und daan, da sie die grünen gründer der bank sind.\n"
+                                   "`bank.take <geld>`\nExample: `bank.take 10000`")
+
+    elif message.content.startswith("bank.donate"):
+        try:
+            _, geld = message.content.split(" ")
+        except ValueError:
+            await message.channel.send("hast mit den arguments gekackt")
+            return
+        try:
+            geld = int(geld)
+        except ValueError:
+            await message.channel.send("integer bitte")
+            return
+
+        await message.channel.send(f"~tradeoffer <@{user_id}> I_get: money: {geld}")
+
+    elif message.content.startswith("bank.take"):
+        if user_id not in (422800248935546880, 649457215564152852):
+            await message.channel.send("blud versucht erstmal die bank auszurauben; bomboclaat")
+            return
+        try:
+            _, geld = message.content.split(" ")
+        except ValueError:
+            await message.channel.send("hast mit den arguments gekackt")
+            return
+        try:
+            geld = int(geld)
+        except ValueError:
+            await message.channel.send("integer bitte")
+            return
+
+        await message.channel.send(f"~tradeoffer <@{user_id}> you_get: money: {geld}")
+
+    elif message.content.startswith("bank.eval"):
+        if user_id != 422800248935546880:
+            await message.channel.send("das passiert mir nicht nochmal; es passiert nicht was passieren muss und es wird folglich auch nicht gefickt")
+            return
+
+        split_message = message.content.split(" ")
+        message_to_send = " ".join(split_message[1:])
+        await message.channel.send(message_to_send)
 
 bot.run(TOKEN)
