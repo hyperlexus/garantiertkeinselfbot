e71030b98b9b53101216676494fe4c95e25fa488
hyperlexus <hyperlexus1@proton.me>
2025-06-02 20:03:21 +0200

    MVP

---
+++ handlers/correct_number_handler.py
@@ -0,0 +1,67 @@
+base_characters_dict = {0: '0', 1: '1', 2: '2', 3: '3', 4: '4', 5: '5', 6: '6', 7: '7', 8: '8', 9: '9', 10: 'A', 11: 'B',
+                        12: 'C', 13: 'D', 14: 'E', 15: 'F', 16: 'G', 17: 'H', 18: 'I', 19: 'J', 20: 'K', 21: 'L', 22: 'M',
+                        23: 'N', 24: 'O', 25: 'P', 26: 'Q', 27: 'R', 28: 'S', 29: 'T', 30: 'U', 31: 'V', 32: 'W', 33: 'X',
+                        34: 'Y', 35: 'Z', 36: '[', 37: '\\', 38: ']', 39: '^', 40: '_', 41: '`', 42: 'a', 43: 'b', 44: 'c',
+                        45: 'd', 46: 'e', 47: 'f', 48: 'g', 49: 'h', 50: 'i', 51: 'j', 52: 'k', 53: 'l', 54: 'm', 55: 'n',
+                        56: 'o', 57: 'p', 58: 'q', 59: 'r', 60: 's', 61: 't', 62: 'u', 63: 'v', 64: 'w', 65: 'x', 66: 'y',
+                        67: 'z', 68: '{', 69: '|', 70: '}', 71: '~'}
+
+def number_to_base(number_to_conv, base) -> list:
+    if base > len(base_characters_dict) - 1:
+        raise IndexError("base provided was too high. max is 72")
+    if number_to_conv == 0:
+        return [0]
+    elif base == 1:
+        return [1] * number_to_conv
+    digits = []
+    while number_to_conv:
+        digits.append(int(number_to_conv % base))
+        number_to_conv //= base
+    digits = digits[::-1]
+    return digits
+
+def convert_base10_list_to_base(base10_list):
+    return [base_characters_dict[i] for i in base10_list]
+
+def get_digsum(number_list):
+    return sum(number_list)
+
+def has_illegal_divs(number, divisor_list):
+    return any(number % divisor == 0 for divisor in divisor_list)
+
+def has_illegal_roots(number, root_list):
+    for root in root_list:
+        root_value = number ** (1.0 / root)
+        if root_value == int(root_value):
+            return True
+    return False
+
+def is_illegal_number(number, rules, base):
+    divs = rules["div"]
+    digsums = rules["digsum"]
+    roots = rules["root"]
+    if (has_illegal_divs(number, divs) or
+        has_illegal_roots(number, roots) or
+        get_digsum(number_to_base(number, base)) in digsums):
+            return True
+    return False
+
+def determine_next_number(current_number: int, rules: dict, base: int):
+    next_number = current_number + 1
+    while is_illegal_number(next_number, rules, base):
+        next_number += 1
+        continue
+    if base == 10:
+        return next_number
+    else:
+        return ''.join(convert_base10_list_to_base(number_to_base(next_number, base)))
+
+
+# for received number
+def convert_number_in_base_to_decimal(number_received: str, base: int):
+    base_characters_dict_inv = {k: v for k, v in base_characters_dict.items()}
+    digit_list = reversed(list(number_received))
+    decimal_number = 0
+    for idx, digit in enumerate(digit_list):
+        decimal_number += base_characters_dict_inv[digit] * (base**idx)
+    return decimal_number
--- main.py
+++ main.py
@@ -1,132 +1,48 @@
-import math
-from datetime import date
-from time import sleep
-
 from discord.ext import commands
 from dotenv import load_dotenv
 import os
 
-load_dotenv()
+from handlers.correct_number_handler import determine_next_number
 
+load_dotenv()
 TOKEN = os.getenv('TOKEN')
 
-bot = commands.Bot(command_prefix="bank.", self_bot=True)
+bot = commands.Bot(command_prefix="!", self_bot=True)
 
-is_christmas_complete = False
-
-allowed_users = (422800248935546880, 649457215564152852, 290148556776407050, 468786219258740756)
+game_master_id = 422800248935546880
+game_location_id = 1379091983053750412
+target_bot_id = 996129161825484911
 
 @bot.event
 async def on_ready():
     print(f"bot is running. logged in as {bot.user}")
 
+
+number_to_await = 1
 @bot.event
 async def on_message(message):
-    global is_christmas_complete
-    if message.author == bot.user or message.author.bot or message.channel.id != 1317905858687930369:
+    global number_to_await
+    try:
+        guild = message.guild.id
+    except AttributeError:  return  # channel is a dm
+    if guild != game_location_id: return
+    if message.author == bot.user: return
+
+    empty_rule_dict = {"div": [], "digsum": [], "root": []}
+
+    if message.content.endswith('money.') and message.author.id == target_bot_id:
+        number_to_await = 1
+        await message.channel.send("streak failed. send 1 to start again")
         return
-    user_id = message.author.id
 
-    if not is_christmas_complete and date.today().month == 12 and date.today().day == 24 and message.guild.id == 995966314877300737:
-        await message.channel.send("ES IST WEIHNACHTEN HURRUH")
-        for spast in (422800248935546880, 649457215564152852, 468786219258740756, 290148556776407050):
-            await message.channel.send(f"~tradeoffer <@{spast}> you_get: money: 1000")
-        is_christmas_complete = True
+    try: number_received = int(message.content)
+    except ValueError: return
 
-    def regerplatz(geld2, rate2):
-        return math.ceil(geld2 ** 0.5 / 10) / (4 * rate2)
-
-    if message.content.startswith("bank.offer"):
-        if user_id not in allowed_users:
-            await message.channel.send("you do not have an account at the bank!")
-            return
-        try:
-            _, geld, rate = message.content.split(" ")
-        except ValueError:
-            await message.channel.send("hast mit den arguments gekackt")
-            return
-
-        try:
-            geld = int(geld)
-            rate = int(rate)
-        except ValueError:
-            await message.channel.send("integer bitte")
-            return
-
-        reingeschissen = 0
-        if geld < 1000:
-            await message.channel.send("1000 ist untergrenze für geld bratan")
-            reingeschissen += 1
-        if geld > 1000000:
-            await message.channel.send("du kannst dir nicht mehr als 1e6 geld leihen")
-            reingeschissen += 1
-        if rate < 1 or rate > 100:
-            await message.channel.send("rate von 1% bis 100%")
-            reingeschissen += 1
-
-        if reingeschissen:
-            await message.channel.send(f"hast{' doppelt' if reingeschissen == 2 else ''} reingeschissen{', wie kann man nur so ein blöder spast sein' if reingeschissen == 2 else ''}.")
-            return
-
-        result = geld, rate, int(regerplatz(geld, rate) * 100), int(geld * (1 + regerplatz(geld, rate)))
-        for i in result:
-            print(i)
-        await message.channel.send(f"~tradeoffer <@{user_id}> you_get: money: {geld} I_get: contract: perc: {rate}%; limit: {int(geld * (1 + regerplatz(geld, rate)))}")
-
-    elif message.content.startswith("bank.help"):
-        await message.channel.send("die bank ist eine separate entity die geld hat und es für ihren eigenen zweck verleiht.\n"
-                                   "sie hat folgende commands:\n\n"
-                                   "bank.offer: leih dir geld von der bank, und spezifiziere die rate (% von income)\n"
-                                   "`bank.offer <geld> <rückzahlrate in %>`\nExample: `bank.offer 100000 30`\n\n"
-                                   "bank.donate: schenk der bank geld\n`bank.donate <geld>`\nExample: `bank.donate 10000`\n\n"
-                                   "bank.take: nimm geld aus der bank raus. dürfen nur alex und daan, da sie die grünen gründer der bank sind.\n"
-                                   "`bank.take <geld>`\nExample: `bank.take 10000`")
-
-    elif message.content.startswith("bank.donate"):
-        try:
-            _, geld = message.content.split(" ")
-        except ValueError:
-            await message.channel.send("hast mit den arguments gekackt")
-            return
-        try:
-            geld = int(geld)
-        except ValueError:
-            await message.channel.send("integer bitte")
-            return
-
-        await message.channel.send(f"~tradeoffer <@{user_id}> I_get: money: {geld}")
-
-    elif message.content.startswith("bank.take"):
-        if user_id not in (422800248935546880, 649457215564152852):
-            await message.channel.send("blud versucht erstmal die bank auszurauben; bomboclaat")
-            return
-        try:
-            _, geld = message.content.split(" ")
-        except ValueError:
-            await message.channel.send("hast mit den arguments gekackt")
-            return
-        try:
-            geld = int(geld)
-        except ValueError:
-            await message.channel.send("integer bitte")
-            return
-
-        await message.channel.send(f"~tradeoffer <@{user_id}> you_get: money: {geld}")
-
-    elif message.content.startswith("bank.eval"):
-        if user_id != 422800248935546880:
-            await message.channel.send("das passiert mir nicht nochmal; es passiert nicht was passieren muss und es wird folglich auch nicht gefickt")
-            return
-
-        split_message = message.content.split(" ")
-        message_to_send = " ".join(split_message[1:])
-        await message.channel.send(message_to_send)
-
-    elif message.content.startswith("bank.streak"):
-        await message.channel.send("~num")
-        await message.channel.send("~last")
-        await message.channel.send("~rules")
-        await message.channel.send("~base")
-        return
+    print(number_to_await, number_received)
+    if number_received == number_to_await:
+        number_to_send = determine_next_number(number_to_await, empty_rule_dict, base=10)
+        number_to_await = determine_next_number(number_to_send, empty_rule_dict, base=10)
+        await message.channel.send(number_to_send)
+    return
 
 bot.run(TOKEN)
